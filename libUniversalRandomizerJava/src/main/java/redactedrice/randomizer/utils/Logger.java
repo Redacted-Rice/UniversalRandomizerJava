fabcd60ff61dabfb3cdbfb60d2e4b925c36bf04f
DasKeifer <43280661+DasKeifer@users.noreply.github.com>
2026-06-15 16:30:52 -0400

    Adding formatting function from GCB logger to URJ Logger

--- libUniversalRandomizerJava/src/main/java/redactedrice/randomizer/utils/Logger.java
+++ libUniversalRandomizerJava/src/main/java/redactedrice/randomizer/utils/Logger.java
@@ -335,6 +335,63 @@ public class Logger {
         return sb.toString();
     }
 
+    // --- ASCII table formatting helpers ---
+
+    public static void findMaxStringLengths(int[] fieldsMaxLengths, String... fields) {
+        if (fieldsMaxLengths.length != fields.length) {
+            throw new IllegalArgumentException("Lengths array length (" + fieldsMaxLengths.length
+                    + ") does not match the passed number of fields (" + fields.length + ")");
+        }
+
+        int idx = 0;
+        for (String field : fields) {
+            if (field.length() > fieldsMaxLengths[idx]) {
+                fieldsMaxLengths[idx] = field.length();
+            }
+            idx++;
+        }
+    }
+
+    public static String createSeparatorLine(int length) {
+        char[] tempArray = new char[length];
+        Arrays.fill(tempArray, '-');
+        return new String(tempArray);
+    }
+
+    public static String createTableTitle(String title, int length) {
+        int spacing = length - title.length() - 2; // -2 for the | at each side
+        StringBuilder tableNameFormatBuilder = new StringBuilder();
+        tableNameFormatBuilder.append("|%");
+        tableNameFormatBuilder.append(spacing / 2);
+        tableNameFormatBuilder.append("s%s%");
+        tableNameFormatBuilder.append(spacing / 2 + spacing % 2); // Handles odd lengths
+        tableNameFormatBuilder.append("s|");
+        return String.format(tableNameFormatBuilder.toString(), "", title, "");
+    }
+
+    public static String createTableFormatString(int[] fieldsMaxLengths, String... formats) {
+        if (fieldsMaxLengths.length < formats.length) {
+            throw new IllegalArgumentException("Lengths array length (" + fieldsMaxLengths.length
+                    + ") is less than the passed number of formats (" + formats.length + ")");
+        }
+
+        Iterator<String> formatItr = Arrays.asList(formats).iterator();
+
+        StringBuilder formatBuilder = new StringBuilder();
+        formatBuilder.append("|");
+        for (int idx = 0; idx < fieldsMaxLengths.length; idx++) {
+            formatBuilder.append("%");
+            if (formatItr.hasNext()) {
+                formatBuilder.append(formatItr.next());
+            }
+            formatBuilder.append(fieldsMaxLengths[idx]);
+            formatBuilder.append("s|");
+        }
+
+        formatBuilder.append("\n");
+        return formatBuilder.toString();
+    }
+
     public static void reset() {
         enabled = true;
         showTimestamp = false;
