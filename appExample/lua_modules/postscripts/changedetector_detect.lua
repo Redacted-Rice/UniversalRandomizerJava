0d1c5f02f849399925371549226a6fd4a73f86fd
DasKeifer <43280661+DasKeifer@users.noreply.github.com>
2026-06-17 12:05:19 -0400

    UPdating appexample for the cahnge detector changes to table format

--- appExample/lua_modules/postscripts/changedetector_detect.lua
+++ appExample/lua_modules/postscripts/changedetector_detect.lua
@@ -12,28 +12,10 @@ return {
 	execute = function(context)
 		local changedetector = require("randomizer").changedetector
 
-		-- Get the changes and log if there are any
 		local changes = changedetector.detectChanges()
 		if changedetector.hasChanges(changes) then
-			local lines = {}
-			for entryName, entryChanges in pairs(changes) do
-				table.insert(lines, "=== Changes detected for " .. entryName .. " ===")
-
-				for identifier, fieldChanges in pairs(entryChanges) do
-					table.insert(lines, identifier)
-
-					for fieldName, change in pairs(fieldChanges) do
-						table.insert(lines, string.format("  %s: %s -> %s", fieldName, change.old, change.new))
-					end
-					table.insert(lines, "")
-				end
-				table.insert(lines, "")
-			end
-
-			-- Log everything as a single string so its easier to read in the log
-			local logString = table.concat(lines, "\n")
-			logger.info(logString)
+			-- Layout comes from monitor setup; this only renders the ASCII table
+			logger.info(changedetector.formatChangesTable(changes))
 		end
 	end,
 }
-
