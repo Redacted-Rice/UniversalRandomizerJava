28fbe9fec24631841ed9708e1eced0f8a0eaf4a7
DasKeifer <43280661+DasKeifer@users.noreply.github.com>
2026-09-02 16:39:32 -0400

    Cleaning and remarking todos

--- libUniversalRandomizerJava/src/main/java/redactedrice/randomizer/lua/arguments/ArgumentType.java
+++ libUniversalRandomizerJava/src/main/java/redactedrice/randomizer/lua/arguments/ArgumentType.java
@@ -1,7 +1,6 @@
 package redactedrice.randomizer.lua.arguments;
 
 // supported argument types for lua module parameters
-// TODO later add support for more basic types
 public enum ArgumentType {
     STRING, INTEGER, DOUBLE, BOOLEAN, ENUM, LIST, TABLE
 }
